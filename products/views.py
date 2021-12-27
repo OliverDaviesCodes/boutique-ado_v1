9876a231c7d5032d63813c0c36b6e47049fe90e4
OliverDaviesCodes <oliverdavieswork@gmail.com>
2021-12-27 09:45:50 +0000

    Added sorting js and back to top link

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
+from django.db.models.functions import Lower
 from .models import Product, Category
 
 # Create your views here.
@@ -66,4 +67,3 @@ def product_detail(request, product_id):
     }
 
     return render(request, 'products/product_detail.html', context)
-    
