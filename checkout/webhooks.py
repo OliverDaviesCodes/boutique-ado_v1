30eef3d0c0de517ea6fd6de1d8e275af57606596
OliverDaviesCodes <oliverdavieswork@gmail.com>
2022-02-04 12:22:23 +0000

    Completed confirmation emails

--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -21,16 +21,17 @@ def webhook(request):
     event = None
 
     try:
-        event = stripe.Webhook.construct_event(
-        payload, sig_header, wh_secret
-        )
+        event = stripe.Webhook.construct_event(payload, sig_header, wh_secret)
     except ValueError as e:
         # Invalid payload
-        return HttpResponse(status=400)
+        print(e)
+        return HttpResponse(content=e, status=400)
     except stripe.error.SignatureVerificationError as e:
         # Invalid signature
-        return HttpResponse(status=400)
+        print(e)
+        return HttpResponse(content=e, status=400)
     except Exception as e:
+        print(e)
         return HttpResponse(content=e, status=400)
 
     # Set up a webhook handler
